042243c089619c2d7d8ef3d2f1be9f36a76a1ac1
Rajan <rajanj448@student.sfit.ac.in>
2024-06-07 18:13:37 +0530

    working

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,6 +1,35 @@
+import { db } from '@/lib/db';
+import { currentUser } from '@clerk/nextjs'
 import React from 'react'
 
-const page = () => {
+type Props = {
+    params: { agencyId: string }
+  }
+
+const page = async({params}:Props) => {
+    const authUser=await currentUser();
+    if(!authUser) return null;
+
+    const userDetails = await db.user.findUnique({
+        where: {
+          email: authUser.emailAddresses[0].emailAddress,
+        },
+      })
+    
+      if (!userDetails) return null
+      const agencyDetails = await db.agency.findUnique({
+        where: {
+          id: params.agencyId,
+        },
+        include: {
+          SubAccount: true,
+        },
+      })
+    
+      if (!agencyDetails) return null
+    
+      const subAccounts = agencyDetails.SubAccount
+
   return (
     <div>page</div>
   )
