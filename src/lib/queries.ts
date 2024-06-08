d11dd6f40e8e2c8b55f5662a933d44437efba7d2
Rajan <rajanj448@student.sfit.ac.in>
2024-06-08 14:43:40 +0530

    - User Details with access control

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -404,3 +404,38 @@ export const getUserPermissions = async (userId: string) => {
     },
   });
 };
+
+
+export const updateUser=async(user:Partial<User>)=>{
+  const response=await db.user.update({
+    where:{
+      email:user.email
+    },
+    data:{...user}
+  })
+
+  return response;
+
+}
+
+export const changeUserPermissions = async (
+  permissionId: string | undefined,
+  userEmail: string,
+  subAccountId: string,
+  permission: boolean
+) => {
+  try {
+    const response = await db.permissions.upsert({
+      where: { id: permissionId },
+      update: { access: permission },
+      create: {
+        access: permission,
+        email: userEmail,
+        subAccountId: subAccountId,
+      },
+    })
+    return response
+  } catch (error) {
+    console.log('🔴Could not change persmission', error)
+  }
+}
