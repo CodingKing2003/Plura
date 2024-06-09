25036e963241042a30b3d28a21ac89646a74818d
Rajan <rajanj448@student.sfit.ac.in>
2024-06-09 13:16:14 +0530

    Team page

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -15,14 +15,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full md:pt-44 pt-20 relative flex items-center justify-center flex-col ">
-        {/* grid */}
+     <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col lg:mb-[300px]">
+    
 
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10 lg:mb-[300px]" />
 
         <p className="text-center">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-          <h1 className="text-9xl font-bold text-center md:text-[300px] mt-32 md:mt-48">
+        <h1 className="text-9xl font-bold text-center lg:mt-[200px] md:text-[300px]">
             Plura
           </h1>
         </div>
@@ -37,7 +37,7 @@ export default function Home() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
-      <section className="flex justify-center items-center flex-col gap-4 md:mt-52 mt-48">
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px] lg:mt-20 ">
         <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
           Our straightforward pricing plans are tailored to meet your needs. If
---
+++ src/components/forms/send-invitation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as z from 'zod';
+
+
+interface SendInvitationProps {
+    agencyId: string
+  }
+
+const SendInvitation = ({agencyId}:SendInvitationProps) => {
+  return (
+    <div>SendInvitation</div>
+  )
+}
+
+export default SendInvitation
