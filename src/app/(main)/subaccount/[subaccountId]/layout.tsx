b125113867fd02e522cc0be1aef16119a61686ce
Rajan <rajanj448@student.sfit.ac.in>
2024-06-10 17:02:59 +0530

    subaccount layout

--- src/app/(main)/subaccount/[subaccountId]/layout.tsx
+++ src/app/(main)/subaccount/[subaccountId]/layout.tsx
@@ -1,27 +1,75 @@
+import InfoBar from '@/components/global/infobar'
+import Sidebar from '@/components/sidebar'
 import Unauthorized from '@/components/unauthorized'
-import { verifyAndAcceptInvitation } from '@/lib/queries'
+import {
+  getAuthUserDetails,
+  getNotificationAndUser,
+  verifyAndAcceptInvitation,
+} from '@/lib/queries'
 import { currentUser } from '@clerk/nextjs'
+import { Role } from '@prisma/client'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {
-    children:React.ReactNode,
-    params:{subaccountId:string}
+  children: React.ReactNode
+  params: { subaccountId: string }
 }
 
-const SubAccountLayout = async(props: Props) => {
+const SubaccountLayout = async ({ children, params }: Props) => {
+  const agencyId = await verifyAndAcceptInvitation()
+  if (!agencyId) return <Unauthorized />
+  const user = await currentUser()
+  if (!user) {
+    return redirect('/')
+  }
 
-    const agencyId=await verifyAndAcceptInvitation();
-    if(!agencyId) return <Unauthorized />
-    const user=await currentUser();
+  let notifications: any = []
 
-    if(!user){
-        return redirect("/")
+  if (!user.privateMetadata.role) {
+    return <Unauthorized />
+  } else {
+    const allPermissions = await getAuthUserDetails()
+    const hasPermission = allPermissions?.Permissions.find(
+      (permissions) =>
+        permissions.access && permissions.subAccountId === params.subaccountId
+    )
+    if (!hasPermission) {
+      return <Unauthorized />
     }
 
-  return (
-    <div>SubAccountLayout</div>
-  )
-}
+    const allNotifications = await getNotificationAndUser(agencyId)
 
-export default SubAccountLayout
+    if (
+      user.privateMetadata.role === 'AGENCY_ADMIN' ||
+      user.privateMetadata.role === 'AGENCY_OWNER'
+    ) {
+      notifications = allNotifications
+    } else {
+      const filteredNoti = allNotifications?.filter(
+        (item) => item.subAccountId === params.subaccountId
+      )
+      if (filteredNoti) notifications = filteredNoti
+    }
+  }
+
+  return (
+    <div className="h-screen overflow-hidden">
+      <Sidebar
+        id={params.subaccountId}
+        type="subaccount"
+      />
+
+      <div className="md:pl-[300px]">
+        <InfoBar
+          notifications={notifications}
+          role={user.privateMetadata.role as Role}
+          subAccountId={params.subaccountId as string}
+        />
+        <div className="relative">{children}</div>
+      </div>
+    </div>
+  )
+} 
+
+export default SubaccountLayout
