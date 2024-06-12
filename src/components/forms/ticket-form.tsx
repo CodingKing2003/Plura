7f240a014f531bf57e2c84e4fa82562969a7e2a4
Rajan <rajanj448@student.sfit.ac.in>
2024-06-12 15:04:57 +0530

    Pipeline ticket completed

--- src/components/forms/ticket-form.tsx
+++ src/components/forms/ticket-form.tsx
@@ -1,25 +1,44 @@
-"use client";
-
+'use client'
+import {
+  getSubAccountTeamMembers,
+  saveActivityLogsNotification,
+  searchContacts,
+  upsertTicket,
+} from '@/lib/queries'
 import { TicketFormSchema, TicketWithTags } from '@/lib/types'
-import React, { useEffect, useRef, useState } from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useModal } from '../providers/modal-provider';
-import { useRouter } from 'next/navigation';
 
-import { z } from 'zod';
-import { Contact, Tag, User } from '@prisma/client';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
-import { getSubAccountTeamMembers, saveActivityLogsNotification, searchContacts, upsertTicket } from '@/lib/queries';
-import { toast } from '../ui/use-toast';
-import { Input } from '../ui/input';
-import { Textarea } from '../ui/textarea';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { CheckIcon, ChevronsUpDownIcon, User2 } from 'lucide-react';
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
-import { Button } from '../ui/button';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Contact, Tag, User } from '@prisma/client'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { toast } from '../ui/use-toast'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '../ui/input'
+import { Textarea } from '../ui/textarea'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
+import { CheckIcon, ChevronsUpDownIcon, User2 } from 'lucide-react'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+import { Button } from '../ui/button'
 import {
   Command,
   CommandEmpty,
@@ -27,105 +46,106 @@ import {
   CommandInput,
   CommandItem,
 } from '../ui/command'
-import { cn } from '@/lib/utils';
-import Loading from '../global/loading';
-import TagCreator from '../global/tag-creator';
+import { cn } from '@/lib/utils'
+import Loading from '../global/loading'
+import TagCreator from '../global/tag-creator'
+import { useModal } from '../providers/modal-provider'
 
 type Props = {
-    laneId: string
-    subaccountId: string
-    getNewTicket: (ticket: TicketWithTags[0]) => void
-  }
+  laneId: string
+  subaccountId: string
+  getNewTicket: (ticket: TicketWithTags[0]) => void
+}
 
-const TicketForm = ({laneId,subaccountId,getNewTicket}:Props) => {
-    const { data: defaultData, setClose } = useModal()
-    const router = useRouter()
-    const [tags, setTags] = useState<Tag[]>([])
-    const [contact, setContact] = useState('')
-    const [search, setSearch] = useState('')
-    const [contactList, setContactList] = useState<Contact[]>([])
-    const saveTimerRef = useRef<ReturnType<typeof setTimeout>>()
-    const [allTeamMembers, setAllTeamMembers] = useState<User[]>([])
-    const [assignedTo, setAssignedTo] = useState(
-      defaultData.ticket?.Assigned?.id || ''
-    )
+const TicketForm = ({ getNewTicket, laneId, subaccountId }: Props) => {
+  const { data: defaultData, setClose } = useModal()
+  const router = useRouter()
+  const [tags, setTags] = useState<Tag[]>([])
+  const [contact, setContact] = useState('')
+  const [search, setSearch] = useState('')
+  const [contactList, setContactList] = useState<Contact[]>([])
+  const saveTimerRef = useRef<ReturnType<typeof setTimeout>>()
+  const [allTeamMembers, setAllTeamMembers] = useState<User[]>([])
+  const [assignedTo, setAssignedTo] = useState(
+    defaultData.ticket?.Assigned?.id || ''
+  )
+  const form = useForm<z.infer<typeof TicketFormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(TicketFormSchema),
+    defaultValues: {
+      name: defaultData.ticket?.name || '',
+      description: defaultData.ticket?.description || '',
+      value: String(defaultData.ticket?.value || 0),
+    },
+  })
+  const isLoading = form.formState.isLoading
 
-    const form = useForm<z.infer<typeof TicketFormSchema>>({
-        mode: 'onChange',
-        resolver: zodResolver(TicketFormSchema),
-        defaultValues: {
-          name: defaultData.ticket?.name || '',
-          description: defaultData.ticket?.description || '',
-          value: String(defaultData.ticket?.value || 0),
-        },
-      })
-      const isLoading = form.formState.isLoading
-
-      useEffect(() => {
-        if (subaccountId) {
-          const fetchData = async () => {
-            const response = await getSubAccountTeamMembers(subaccountId)
-            if (response) setAllTeamMembers(response)
-          }
-          fetchData()
-        }
-      }, [subaccountId])
-
-      useEffect(()=>{
-        if(defaultData.ticket){
-          form.reset({
-            name: defaultData.ticket.name || '',
-            description: defaultData.ticket?.description || '',
-            value: String(defaultData.ticket?.value || 0),
-          })
-          if(defaultData.ticket.customerId){
-            setContact(defaultData.ticket.customerId)
-          }
-          const fetchData=async()=>{
-            const reponse=await searchContacts(defaultData.ticket?.Customer.name);
-            setContactList(reponse)
-          }
-          fetchData();
-        }
-
-      },[defaultData])
-
-      const onSubmit = async (values: z.infer<typeof TicketFormSchema>) => {
-        if (!laneId) return
-        try {
-          const response = await upsertTicket(
-            {
-              ...values,
-              laneId,
-              id: defaultData.ticket?.id,
-              assignedUserId: assignedTo,
-              ...(contact ? { customerId: contact } : {}),
-            },
-            tags
-          )
-    
-          await saveActivityLogsNotification({
-            agencyId: undefined,
-            description: `Updated a ticket | ${response?.name}`,
-            subaccountId,
-          })
-    
-          toast({
-            title: 'Success',
-            description: 'Saved  details',
-          })
-          if (response) getNewTicket(response)
-          router.refresh()
-        } catch (error) {
-          toast({
-            variant: 'destructive',
-            title: 'Oppse!',
-            description: 'Could not save pipeline details',
-          })
-        }
-        setClose()
+  useEffect(() => {
+    if (subaccountId) {
+      const fetchData = async () => {
+        const response = await getSubAccountTeamMembers(subaccountId)
+        if (response) setAllTeamMembers(response)
       }
+      fetchData()
+    }
+  }, [subaccountId])
 
+  useEffect(() => {
+    if (defaultData.ticket) {
+      form.reset({
+        name: defaultData.ticket.name || '',
+        description: defaultData.ticket?.description || '',
+        value: String(defaultData.ticket?.value || 0),
+      })
+      if (defaultData.ticket.customerId)
+        setContact(defaultData.ticket.customerId)
+
+      const fetchData = async () => {
+        const response = await searchContacts(
+          //@ts-ignore
+          defaultData.ticket?.Customer?.name
+        )
+        setContactList(response)
+      }
+      fetchData()
+    }
+  }, [defaultData])
+
+  const onSubmit = async (values: z.infer<typeof TicketFormSchema>) => {
+    if (!laneId) return
+    try {
+      const response = await upsertTicket(
+        {
+          ...values,
+          laneId,
+          id: defaultData.ticket?.id,
+          assignedUserId: assignedTo,
+          ...(contact ? { customerId: contact } : {}),
+        },
+        tags
+      )
+
+      await saveActivityLogsNotification({
+        agencyId: undefined,
+        description: `Updated a ticket | ${response?.name}`,
+        subaccountId,
+      })
+
+      toast({
+        title: 'Success',
+        description: 'Saved  details',
+      })
+      if (response) getNewTicket(response)
+      router.refresh()
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Oppse!',
+        description: 'Could not save pipeline details',
+      })
+    }
+    setClose()
+  }
 
   return (
     <Card className="w-full">
--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -781,3 +781,13 @@ export const upsertTag = async (
 
   return response
 }
+
+export const deleteTicket = async (ticketId: string) => {
+  const response = await db.ticket.delete({
+    where: {
+      id: ticketId,
+    },
+  })
+
+  return response
+}
