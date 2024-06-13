665a6c80680557214ca2c1dd1d9410a06e93970b
Rajan <rajanj448@student.sfit.ac.in>
2024-06-13 23:55:56 +0530

    FunnelId

---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-settings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FunnelSettings = () => {
+  return (
+    <div>FunnelSettings</div>
+  )
+}
+
+export default FunnelSettings
---
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/_components/funnel-steps.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FunnelSteps = () => {
+  return (
+    <div>FunnelSteps</div>
+  )
+}
+
+export default FunnelSteps
